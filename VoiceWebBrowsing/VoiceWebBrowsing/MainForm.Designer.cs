f17c6c8624a7793a3ed5ca3bd63058823acf2b7b
Sebastian Wain <sebastian.wain@nektra.com>
2011-11-02 11:27:39 -0300

    first commit

---
+++ VoiceWebBrowsing/VoiceWebBrowsing/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace VoiceWebBrowsing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CommandTextBox = new System.Windows.Forms.TextBox();
+            this.ReceiveHackerNewsButton = new System.Windows.Forms.Button();
+            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ArticleNumberUpDown = new System.Windows.Forms.NumericUpDown();
+            this.ReadArticleButton = new System.Windows.Forms.Button();
+            this.SaveArticleButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ArticleNumberUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voice Command";
+            // 
+            // CommandTextBox
+            // 
+            this.CommandTextBox.Location = new System.Drawing.Point(115, 27);
+            this.CommandTextBox.Name = "CommandTextBox";
+            this.CommandTextBox.ReadOnly = true;
+            this.CommandTextBox.Size = new System.Drawing.Size(453, 20);
+            this.CommandTextBox.TabIndex = 1;
+            // 
+            // ReceiveHackerNewsButton
+            // 
+            this.ReceiveHackerNewsButton.Location = new System.Drawing.Point(6, 27);
+            this.ReceiveHackerNewsButton.Name = "ReceiveHackerNewsButton";
+            this.ReceiveHackerNewsButton.Size = new System.Drawing.Size(135, 20);
+            this.ReceiveHackerNewsButton.TabIndex = 2;
+            this.ReceiveHackerNewsButton.Text = "ReceiveHackerNews";
+            this.ReceiveHackerNewsButton.UseVisualStyleBackColor = true;
+            this.ReceiveHackerNewsButton.Click += new System.EventHandler(this.ReceiveHackerNewsButton_Click);
+            // 
+            // BackgroundWorker
+            // 
+            this.BackgroundWorker.WorkerSupportsCancellation = true;
+            this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(486, 27);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(50, 20);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SaveArticleButton);
+            this.groupBox1.Controls.Add(this.ArticleNumberUpDown);
+            this.groupBox1.Controls.Add(this.ReadArticleButton);
+            this.groupBox1.Controls.Add(this.ReceiveHackerNewsButton);
+            this.groupBox1.Controls.Add(this.StopButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(553, 68);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Manual Command";
+            // 
+            // ArticleNumberUpDown
+            // 
+            this.ArticleNumberUpDown.Location = new System.Drawing.Point(406, 27);
+            this.ArticleNumberUpDown.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.ArticleNumberUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.ArticleNumberUpDown.Name = "ArticleNumberUpDown";
+            this.ArticleNumberUpDown.Size = new System.Drawing.Size(47, 20);
+            this.ArticleNumberUpDown.TabIndex = 5;
+            this.ArticleNumberUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // ReadArticleButton
+            // 
+            this.ReadArticleButton.Location = new System.Drawing.Point(154, 27);
+            this.ReadArticleButton.Name = "ReadArticleButton";
+            this.ReadArticleButton.Size = new System.Drawing.Size(118, 20);
+            this.ReadArticleButton.TabIndex = 4;
+            this.ReadArticleButton.Text = "Read Article #";
+            this.ReadArticleButton.UseVisualStyleBackColor = true;
+            this.ReadArticleButton.Click += new System.EventHandler(this.ReadArticleButton_Click);
+            // 
+            // SaveArticleButton
+            // 
+            this.SaveArticleButton.Location = new System.Drawing.Point(278, 27);
+            this.SaveArticleButton.Name = "SaveArticleButton";
+            this.SaveArticleButton.Size = new System.Drawing.Size(122, 20);
+            this.SaveArticleButton.TabIndex = 6;
+            this.SaveArticleButton.Text = "Save Article #";
+            this.SaveArticleButton.UseVisualStyleBackColor = true;
+            this.SaveArticleButton.Click += new System.EventHandler(this.SaveArticleButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 138);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CommandTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Voice Web Browsing";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ArticleNumberUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CommandTextBox;
+        private System.Windows.Forms.Button ReceiveHackerNewsButton;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown ArticleNumberUpDown;
+        private System.Windows.Forms.Button ReadArticleButton;
+        private System.Windows.Forms.Button SaveArticleButton;
+    }
+}
+
---
+++ VoiceWebBrowsing/VoiceWebBrowsing/MainForm.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+
+
+namespace VoiceWebBrowsing
+{
+    public partial class MainForm : Form
+    {
+        #region Constants
+        //const string _bogusRSSFeed = "<items><item><title>First title</title><link>http://</link></item><item><title>Second title</title><link>http://</link></item></items>";
+        const string _bogusRSSFeed = null;
+        const string downloadPath = @"..\..\..\..\Download";
+        #endregion
+
+        #region Private Variables
+        SpeechRecognizer _speechRecognizer = new SpeechRecognizer();
+        SpeechSynthesizer _ttsVoice = new SpeechSynthesizer();
+        SpeechAudioFormatInfo formatInfo = new SpeechAudioFormatInfo(8000, AudioBitsPerSample.Sixteen, AudioChannel.Mono);
+        Queue<string> _queue = new Queue<string>();
+        List<string> articleList = new List<string>();
+        HashSet<SpeechSynthesizer> tts2FileTasks = new HashSet<SpeechSynthesizer>();
+        #endregion
+
+        #region Private Methods
+        private void InitGrammar()
+        {
+            GrammarBuilder readGrammar = new Choices(new string[] { "read article" });
+            Choices articleChoice = new Choices();
+            for (int i = 1; i <= 30; i++)
+            {
+                articleChoice.Add(i.ToString());
+            }
+            readGrammar.Append(articleChoice);
+
+            GrammarBuilder saveGrammar = new Choices(new string[] { "save article" });
+            saveGrammar.Append(articleChoice);
+
+            GrammarBuilder otherGrammar = new Choices(new string[] { "receive hacker news", "stop", "test" });
+
+            //GrammarBuilder commands = new Choices(new string[] { "receive hacker news", "stop", "test" });
+            Choices commands = new Choices();
+            commands.Add(new Choices(new GrammarBuilder[] { readGrammar, saveGrammar, otherGrammar }));
+
+            var grammar = new Grammar(commands);
+            this._speechRecognizer.LoadGrammar(grammar);
+        }
+
+        private void Say(string text)
+        {
+            this._ttsVoice.SpeakAsync(text);
+        }
+
+        private void ReadHackerNewsFeed()
+        {
+            string hackerNewsRSSUrl = "http://news.ycombinator.com/rss";
+
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                //
+                // an app.config is added to surpress: The server committed a protocol violation. Section=ResponseStatusLine
+                //
+                string rssXmlStr = null;
+                if (_bogusRSSFeed == null)
+                    rssXmlStr = client.DownloadString(hackerNewsRSSUrl);
+                else
+                    rssXmlStr = _bogusRSSFeed;
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(rssXmlStr);
+
+                XmlNodeList items = xmlDoc.SelectNodes("//item");
+
+                int counter = 1;
+                articleList.Clear();
+                foreach (XmlNode item in items)
+                {
+                    string title = item.SelectSingleNode("title").InnerText;
+                    string link = item.SelectSingleNode("link").InnerText;
+                    articleList.Add(link);
+                    Say("article " + counter.ToString() + " " + title);
+                    System.Diagnostics.Debug.WriteLine(title);
+
+                    counter++;
+                }
+            }
+        }
+
+        private void ReceiveHackerNewsButton_Click(object sender, EventArgs e)
+        {
+            ReceiveHackerNewsCommand();
+        }
+
+        private void SaveArticle(string link, string article)
+        {
+            SpeechSynthesizer tts2File = new SpeechSynthesizer();
+            tts2File.SpeakStarted += new EventHandler<SpeakStartedEventArgs>(tts2File_SpeakStarted);
+            tts2File.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(tts2File_SpeakCompleted);
+            System.Security.Cryptography.SHA1Managed hashAlgorithm = new System.Security.Cryptography.SHA1Managed();
+            hashAlgorithm.Initialize();
+            byte[] buffer = Encoding.UTF8.GetBytes(link);
+            byte[] hash = hashAlgorithm.ComputeHash(buffer);
+            string fileName = BitConverter.ToString(hash).Replace("-", string.Empty) + ".wav";
+            string executionPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string fullPath = Path.Combine(executionPath, downloadPath, fileName);
+            hashAlgorithm.Clear(); 
+            tts2File.SetOutputToWaveFile(fullPath, formatInfo);
+            tts2File.SpeakAsync(article);
+            this.tts2FileTasks.Add(tts2File);
+        }
+
+        #endregion
+
+        #region Constructor
+        public MainForm()
+        {
+            InitializeComponent();
+            _speechRecognizer.Enabled = true;
+            _speechRecognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_speechRecognizer_SpeechRecognized);
+        }
+        #endregion
+
+        #region Events
+        void _ttsVoice_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            this._ttsVoice.SetOutputToNull(); // Needed for flushing file buffers.
+        }
+
+        void _speechRecognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string command = e.Result.Text;
+            CommandTextBox.Text = command;
+
+            if (command == "test")
+            {
+                return;
+            }
+
+            if (command == "stop")
+            {
+                StopCommand();
+
+                return;
+            }
+
+            if (command == "receive hacker news")
+            {
+                ReceiveHackerNewsCommand();
+
+                return;
+            }
+
+            if (command.Contains("read article"))
+            {
+                string[] words = command.Split(' ');
+
+                ReadArticleCommand(Decimal.Parse(words[2]));
+
+                return;
+            }
+
+            if (command.Contains("save article"))
+            {
+                string[] words = command.Split(' ');
+
+                SaveArticleCommand(Decimal.Parse(words[2]));
+
+                return;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitGrammar();
+            BackgroundWorker.RunWorkerAsync();
+        }
+        
+        void tts2File_SpeakStarted(object sender, SpeakStartedEventArgs e)
+        {
+        }
+
+        void tts2File_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            SpeechSynthesizer tts2File = (SpeechSynthesizer)sender;
+
+            tts2File.SetOutputToNull();
+            this.tts2FileTasks.Remove(tts2File);
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StopCommand();
+        }
+        private void ReadArticleButton_Click(object sender, EventArgs e)
+        {
+            ReadArticleCommand(ArticleNumberUpDown.Value);
+        }
+
+        private void SaveArticleButton_Click(object sender, EventArgs e)
+        {
+            SaveArticleCommand(ArticleNumberUpDown.Value);            
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(125);
+
+                lock(this)
+                {
+                    if(this._queue.Count > 0)
+                    {
+                        string cmd = this._queue.Dequeue();
+
+                        if (cmd != null)
+                        {
+                            System.Uri uri = new Uri(cmd);
+                            if (uri.Scheme == "voicewebbrowsing")
+                            {
+                                if (uri.Host == "receivehackernews")
+                                {
+                                    ReadHackerNewsFeed();
+                                }
+                                else if (uri.Host == "stop")
+                                {
+                                    this._ttsVoice.SpeakAsyncCancelAll();
+                                }
+                                else if (uri.Host == "readarticle" || uri.Host == "savearticle")
+                                {
+                                    string articleNumberStr = System.IO.Path.GetFileName(uri.AbsolutePath);
+                                    int articleNumber = int.Parse(articleNumberStr);
+
+                                    if (articleNumber > articleList.Count)
+                                    {
+                                        Say("please retrieve hacker news articles first");
+                                    }
+                                    else
+                                    {
+                                        articleNumber--; // 0-based index
+                                        string link = articleList[articleNumber];
+
+                                        java.net.URL url = new java.net.URL(link);
+                                        string article = de.l3s.boilerpipe.extractors.ArticleExtractor.INSTANCE.getText(url);
+                                        if (uri.Host == "readarticle")
+                                        {
+                                            Say(article);
+                                        }
+                                        else if (uri.Host == "savearticle")
+                                        {
+                                            SaveArticle(link, article);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Commands
+        private void ReceiveHackerNewsCommand()
+        {
+            StopCommand();
+            lock (this)
+            {
+                this._queue.Enqueue("voicewebbrowsing://receivehackernews");
+            }
+        }
+        private void StopCommand()
+        {
+            lock (this)
+            {
+                this._queue.Enqueue("voicewebbrowsing://stop");
+            }
+        }
+
+        private void ReadArticleCommand(Decimal article)
+        {
+            StopCommand();
+            lock (this)
+            {
+                this._queue.Enqueue(String.Format("voicewebbrowsing://readarticle/{0}", article.ToString()));
+            }            
+        }
+
+        private void SaveArticleCommand(decimal article)
+        {
+            lock (this)
+            {
+                this._queue.Enqueue(String.Format("voicewebbrowsing://savearticle/{0}", article.ToString()));
+            }
+        }
+        #endregion
+    }
+}
